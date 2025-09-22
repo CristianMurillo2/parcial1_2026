61513103878717d825ff6272470b4f5281ae3af2
JuanFe02 <juanf.perezs@udea.edu.co>
2025-09-21 19:39:22 -0500

    desencriptacion

---
+++ Desafio1/encriptacion.h
@@ -0,0 +1,6 @@
+#ifndef ENCRIPTACION_H
+#define ENCRIPTACION_H
+
+unsigned char* j_desencriptarMensaje(unsigned char* j_dataEncriptada, unsigned int j_tamano, int j_claveK, int j_rotacionN);
+
+#endif // ENCRIPTADO_H
